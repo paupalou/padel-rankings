f489f30b5c4d02fee1a1a1ebe225d3d77b6a69d6
Pau Palou <pau.palou@nccgroup.com>
2023-11-03 14:45:46 +0100

    try with module

--- migrations/1698663633481-ranking_public_prop.ts
+++ migrations/1698663633481-ranking_public_prop.ts
@@ -4,10 +4,13 @@ import { Ranking } from "types";
 const filename = new URL('', import.meta.url).pathname;
 const atomic = db.atomic();
 
-for await (const res of db.list<Ranking>({ prefix: ["rankings"] })) {
-  atomic.set(res.key, { ...res.value, private: true });
+export default {
+   run: async () => {
+    for await (const res of db.list<Ranking>({ prefix: ["rankings"] })) {
+      atomic.set(res.key, { ...res.value, private: true });
+    }
+
+    await atomic.commit();
+    return `executed database migration ${filename}`
+  }
 }
-
-
-await atomic.commit();
-console.debug(`executed database migration ${filename}`)
