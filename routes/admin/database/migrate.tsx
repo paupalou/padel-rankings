3bf1be23b8faf72614b93e33e521bc54f867e0d4
Pau Palou <pau.palou@nccgroup.com>
2023-11-03 14:40:50 +0100

    try with relative route

--- routes/admin/database/migrate.tsx
+++ routes/admin/database/migrate.tsx
@@ -41,7 +41,7 @@ export const handler: Handlers = {
     }
 
     for (const file of files) {
-      await import(`migrations/${file}`);
+      await import(`../../../migrations/${file}`);
       lastMigration = file;
     }
 
